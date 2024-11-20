81addb0212cd2206c3debd2f63e2860b95e5cb36
vlad <vlad@triumpharcade.com>
2024-11-19 18:07:43 -0800

    Initial Commit

---
+++ ExampleTests/ExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleTests.swift
+//  ExampleTests
+//
+//  Created by Vladyslav Vcherashnii on 11/19/24.
+//
+
+import Testing
+@testable import Example
+
+struct ExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
